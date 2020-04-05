7e62497b7a32dc7b9f1d6b5575608e93b5d1387e
joonvena <joonvena@gmail.com>
2020-04-05 11:02:17 +0300

    Added tablewriter library and function to fetch bucket  region

--- README.md
+++ README.md
@@ -2,21 +2,20 @@
 
 Simple tools to manage S3-buckets. Application is written in Go and uses [AWS SDK For Go](https://docs.aws.amazon.com/sdk-for-go/) to interact with S3.
 
-Currently it can only list & delete buckets. You can provide full bucket name to be deleted or you can provide partial match that returns all the buckets matching the name. This delete command will also delete buckets that are not empty.
-
 ## Installation
 
 If you want to build it yourself:
 
 ```
 go get -u github.com/aws/aws-sdk-go/...
+go get github.com/olekukonko/tablewriter
 # Windows users
 go build -o s3.exe
 # Unix
 go build -o s3
 ```
 
-Or you can use the provided already compiled binaries
+Or you can download the binaries from [Releases](https://github.com/joonvena/S3_CLI/releases)
 
 ## Usage
 
@@ -29,3 +28,10 @@ Or you can use the provided already compiled binaries
 `s3 delete -profile <profile> -bucket <bucket name> -region <region>`
 
 ![Image description](delete.gif)
+
+## Changes
+
+###### 05.04.2020
+
+- Added tablewriter library to print out the bucket list nicely.
+- Region is now displayed when buckets are listed
--- main.go
+++ main.go
@@ -2,36 +2,85 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/olekukonko/tablewriter"
 )
+
+type Bucket struct {
+	Name   string `json:"name"`
+	Region string `json:"region"`
+}
 
 func exitErrorf(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 	os.Exit(1)
 }
 
-func listBuckets(svc *s3.S3, err error) {
+func getBucketRegion(svc *s3.S3, bucket *string) string {
 
+	ctx := context.Background()
+	region, err := s3manager.GetBucketRegionWithClient(ctx, svc, *bucket)
+
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == "NotFound" {
+			fmt.Fprintf(os.Stderr, "unable to find bucket %s's region not found\n", *bucket)
+		}
+		fmt.Println("Error happened", err)
+	}
+	return region
+}
+
+func listBuckets(svc *s3.S3, err error, region *string) {
+	var allContent []Bucket
 	result, err := svc.ListBuckets(nil)
 	if err != nil {
 		exitErrorf("Unable to list buckets, %v", err)
 	}
 
 	for _, b := range result.Buckets {
-		fmt.Printf("%v %v\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+		//fmt.Printf("%v %v\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+		// getBucketRegion(svc, b.Name)
+
+		input := []byte(fmt.Sprintf(`[{
+			"name": "%v",
+			"region": "%v"	
+		}]`, aws.StringValue(b.Name), getBucketRegion(svc, b.Name)))
+
+		var tmpBuckets []Bucket
+		err := json.Unmarshal(input, &tmpBuckets)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		allContent = append(allContent, tmpBuckets...)
+
 	}
+
+	// Create table to display buckets
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Region"})
+
+	for _, v := range allContent {
+		table.Append([]string{v.Name, v.Region})
+	}
+
+	table.Render()
 }
 
 func deleteBuckets(svc *s3.S3, err error, region *string, bucket *string) {
-	var bucketsToBeDeleted []string
+	var allContent []Bucket
 
 	result, err := svc.ListBuckets(nil)
 	if err != nil {
@@ -40,12 +89,30 @@ func deleteBuckets(svc *s3.S3, err error, region *string, bucket *string) {
 
 	for _, b := range result.Buckets {
 		if strings.Contains(aws.StringValue(b.Name), *bucket) {
-			bucketsToBeDeleted = append(bucketsToBeDeleted, aws.StringValue(b.Name))
+			input := []byte(fmt.Sprintf(`[{
+				"name": "%v",
+				"region": "%v"	
+			}]`, aws.StringValue(b.Name), getBucketRegion(svc, b.Name)))
+
+			var tmpBuckets []Bucket
+			err := json.Unmarshal(input, &tmpBuckets)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			allContent = append(allContent, tmpBuckets...)
 		}
 	}
 
-	stringSlices := strings.Join(bucketsToBeDeleted[:], "\n")
-	fmt.Print(stringSlices)
+	// Create table to display buckets
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Region"})
+
+	for _, v := range allContent {
+		table.Append([]string{v.Name, v.Region})
+	}
+
+	table.Render()
 
 	fmt.Println("\n\nAre you sure you want to delete these buckets and their content? (y/n)")
 
@@ -58,32 +125,32 @@ func deleteBuckets(svc *s3.S3, err error, region *string, bucket *string) {
 	}
 
 	if char == 121 {
-		for _, bucket := range bucketsToBeDeleted {
+		for _, bucket := range allContent {
 			iter := s3manager.NewDeleteListIterator(svc, &s3.ListObjectsInput{
-				Bucket: aws.String(bucket),
+				Bucket: aws.String(bucket.Name),
 			})
 
 			if err := s3manager.NewBatchDeleteWithClient(svc).Delete(aws.BackgroundContext(), iter); err != nil {
-				exitErrorf("Unable to delete objects from bucket %q, %v", bucket, err)
+				exitErrorf("Unable to delete objects from bucket %q, %v", bucket.Name, err)
 			}
-			fmt.Printf("Deleted object(s) from bucket: %v", bucket)
+			fmt.Printf("Deleted object(s) from bucket: %v", bucket.Name)
 
 			_, err = svc.DeleteBucket(&s3.DeleteBucketInput{
-				Bucket: aws.String(bucket),
+				Bucket: aws.String(bucket.Name),
 			})
 			if err != nil {
-				exitErrorf("Unable to delete bucket %q, %v", bucket, err)
+				exitErrorf("Unable to delete bucket %q, %v", bucket.Name, err)
 			}
 
 			// Wait until bucket is deleted before finishing
-			fmt.Printf(" Waiting for bucket %q to be deleted...\n", bucket)
+			fmt.Printf(" Waiting for bucket %q to be deleted...\n", bucket.Name)
 
 			err = svc.WaitUntilBucketNotExists(&s3.HeadBucketInput{
-				Bucket: aws.String(bucket),
+				Bucket: aws.String(bucket.Name),
 			})
 		}
 	} else {
-		fmt.Println("Wrong input provided!")
+		fmt.Println("Deletion cancelled!")
 	}
 
 }
@@ -115,7 +182,7 @@ func main() {
 	switch os.Args[1] {
 
 	case "list":
-		listBuckets(svc, err)
+		listBuckets(svc, err, region)
 
 	case "delete":
 		deleteBuckets(svc, err, region, bucket)
